bb4ab75295ca00861ca9747efb7f2de45bd7b997
Pieter Huybrechts <huybrechtspieter@gmail.com>
2025-06-12 12:09:59 +0200

    Add alias to easily find app identifiers  of apps

---
+++ .zshrc.d/aliases.zsh
@@ -0,0 +1,1 @@
+alias app-id='ls /Applications | fzf | xargs -I{} cat /Applications/{}/Contents/Info.plist | grep CFBundleIdentifier -A1 | tail -1 | sed "s/<\/\?string>//g" | xargs -I{} sh -c "echo \"{}\nhas been coppied to your pastebuffer\" && echo {}|pbcopy"'
